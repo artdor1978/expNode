2b09676b61682d0f67e04aff725a0c2a3e0bd10d
ArtDor1978 <artdor1978@gmail.com>
2020-11-30 16:12:24 +0200

    дошел до зомби

--- meadowlark.js
+++ meadowlark.js
@@ -8,12 +8,26 @@ var handlebars = require("express-handlebars").create({
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", process.env.PORT || 3000);
+app.use(function (req, res, next) {
+	res.locals.showTests =
+		app.get("env") !== "production" && req.query.test === "1";
+	next();
+});
 app.use(express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
 	res.render("home");
 });
-app.get('/about', function(req, res) {
-res.render('about', { fortune: fortune.getFortune() } );
+app.get("/about", function (req, res) {
+	res.render("about", {
+		fortune: fortune.getFortune(),
+		pageTestScript: "/qa/tests-about.js",
+	});
+});
+app.get("/tours/hood-river", function (req, res) {
+	res.render("tours/hood-river");
+});
+app.get("/tours/request-group-rate", function (req, res) {
+	res.render("tours/request-group-rate");
 });
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function (req, res, next) {
